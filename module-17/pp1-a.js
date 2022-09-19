1c9e0e79937b11d15c0d6a405a53d0b0f568144f
codernowrin29 <codernowrin29@gmail.com>
2022-09-19 13:04:39 +0600

    1st and 2nd problem solved with JS

---
+++ module-17/pp1-a.js
@@ -0,0 +1,5 @@
+//Find the index of 'Banana and replace 'Banana' with 'Mango'.
+
+var fruits = ['Apple', 'Banana', 'Orange'];
+fruits[1] = 'Mango';
+console.log(fruits);
---
+++ module-17/pp1-b.js
@@ -0,0 +1,6 @@
+//Remove 'Orange' and add 'Watermelon'.
+
+var fruits = ['Apple', 'Banana', 'Orange'];
+fruits.pop();
+fruits.push('Watermelon');
+console.log(fruits);
