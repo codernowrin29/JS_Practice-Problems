9ba0e856c6ff16e4d478ff9c845930664febf9c2
codernowrin29 <codernowrin29@gmail.com>
2022-09-19 16:36:28 +0600

    3rd problem solved in pp2

---
+++ module-17/pp2.js
@@ -0,0 +1,120 @@
+//Write a program to find your and your friend's grades using if-else.
+
+//1) My Score
+var Me = 85;
+
+if (Me >= 80) {
+    console.log("Me = Grade A");
+}
+
+else if (Me >= 60) {
+    console.log("Me = Grade B");
+}
+
+else if (Me >= 50) {
+    console.log("Me = Grade C");
+}
+
+else if (Me >= 40) {
+    console.log("Me = Grade D");
+}
+
+else {
+    console.log("Me = Grade F");
+}
+
+
+//2) Tom's Score
+var Tom = 66;
+
+if (Tom >= 80) {
+    console.log("Tom = Grade A");
+}
+
+else if (Tom >= 60) {
+    console.log("Tom = Grade B");
+}
+
+else if (Tom >= 50) {
+    console.log("Tom = Grade C");
+}
+
+else if (Tom >= 40) {
+    console.log("Tom = Grade D");
+}
+
+else {
+    console.log("Tom = Grade F");
+}
+
+
+//3) Jane's Score
+var Jane = 95;
+
+if (Jane >= 80) {
+    console.log("Jane = Grade A");
+}
+
+else if (Jane >= 60) {
+    console.log("Jane = Grade B");
+}
+
+else if (Jane >= 50) {
+    console.log("Jane = Grade C");
+}
+
+else if (Jane >= 40) {
+    console.log("Jane = Grade D");
+}
+
+else {
+    console.log("Jane = Grade F");
+}
+
+
+//4) Peter's Score
+var Peter = 56;
+
+if (Peter >= 80) {
+    console.log("Peter = Grade A");
+}
+
+else if (Peter >= 60) {
+    console.log("Peter = Grade B");
+}
+
+else if (Peter >= 50) {
+    console.log("Peter = Grade C");
+}
+
+else if (Peter >= 40) {
+    console.log("Peter = Grade D");
+}
+
+else {
+    console.log("Peter = Grade F");
+}
+
+
+//5) John's Score
+var John = 40;
+
+if (John >= 80) {
+    console.log("John = Grade A");
+}
+
+else if (John >= 60) {
+    console.log("John = Grade B");
+}
+
+else if (John >= 50) {
+    console.log("John = Grade C");
+}
+
+else if (John >= 40) {
+    console.log("John = Grade D");
+}
+
+else {
+    console.log("John = Grade F");
+}
