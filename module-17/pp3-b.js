6385c8fb68d7cccb5f15168de334ec1ea62d0a9d
codernowrin29 <codernowrin29@gmail.com>
2022-09-21 22:07:40 +0600

    4th problem solved in pp3

--- module-17/pp3-b.js
+++ module-17/pp3-b.js
@@ -1,3 +1,10 @@
 //Write a program to check whether a triangle is Isosceles or not by using if-else.
 
-//This program have not done yet!!
+var side1 = 9, side2 = 8, side3 = 9;
+if (side1 == side2 || side1 == side3 || side2 == side3) {
+    console.log("Isosceles");
+}
+
+else {
+    console.log("Not Isosceles");
+}
